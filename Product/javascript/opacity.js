6cc068814458b3b6286614ee8fc6f3e9be979564
TrotskyLeon <leo.h.m@mail.ugm.ac.id>
2022-05-28 15:25:48 +0700

    Uploaded to Github

---
+++ Product/javascript/opacity.js
@@ -0,0 +1,7 @@
+$(window).scroll(function(){
+    "use strict"; $(".caption").css("opacity", 1 - $(window).scrollTop() / 250);
+  });
+
+$(window).scroll(function(){
+    "use strict"; $(".res").css("opacity", 9.8 - $(window).scrollTop() / 250);
+  });
